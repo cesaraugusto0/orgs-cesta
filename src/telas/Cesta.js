b9b63c0e39aa9a1708741c425f7ac3c122cd4329
augusto <cesar.cardoso@eb.mil.br>
2023-10-27 15:17:17 -0300

    fonte externa

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -45,7 +45,7 @@ const estilos = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontWeight: "bold",
+        fontFamily: "MontserratBold",
     },
     fazenda: {
         flexDirection: "row",
@@ -59,6 +59,7 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
+        fontFamily: "MontserratRegular",
     },
     descricao : {
         color: "#a3a3a3",
